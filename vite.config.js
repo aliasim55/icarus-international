5580a3dfaecc077b10113845926d1a71aaab8ee1
aliasim55 <aliasim.core.dev@gmail.com>
2026-05-30 03:24:16 +0500

    Add Netlify deployment config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/icarus-international/',
+  base: process.env.NETLIFY ? '/' : '/icarus-international/',
   plugins: [react()]
 });
